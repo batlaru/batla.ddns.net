697cf3c68d22777efac1f49f3070b8d3309f46d5
lsho29ru <166958405+lsho29ru@users.noreply.github.com>
2024-07-22 23:59:26 +0300

    Add files via upload

---
+++ test/cache/get_news.php
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="iso-8859-1"?>
+<document>
+<News news_id="0" title="Dinton news title" text="Dinton test news" photo="i/news/cheetos.jpg" photo_url="//www.youtube.com/embed/6bmwtHEfEFs?autoplay=1" creation_time="2014-09-01 00:00:00"></News>
+</document>
---
+++ test/cache/get_rating_data.php
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="utf-8"?>
+<document>
+  <User id="first" login="Admin" class="din_class__int" avatar="din_avatar__string" level="1" expirience="999,555" clan_id="din_clan_id__string" clan_name="din_clan_name__string" clan_icon="din_clan_icon__string">
+  </User>
+  <MaxRate val="999,555">
+  </MaxRate>
+  <Season league_0_prize_icon="din_SeasonAttr" league_0_prize_title="din_SeasonAttr" league_0_prize_desc="din_SeasonAttr" league_1_prize_icon="din_SeasonAttr" league_1_prize_title="din_SeasonAttr" league_1_prize_desc="din_SeasonAttr" league_2_prize_icon="din_SeasonAttr" league_2_prize_title="din_SeasonAttr" league_2_prize_desc="din_SeasonAttr" league_3_prize_icon="din_SeasonAttr" league_3_prize_title="din_SeasonAttr" league_3_prize_desc="din_SeasonAttr" league_4_prize_icon="din_SeasonAttr" league_4_prize_title="din_SeasonAttr" league_4_prize_desc="din_SeasonAttr" league_5_prize_icon="din_SeasonAttr" league_5_prize_title="din_SeasonAttr" league_5_prize_desc="din_SeasonAttr" end="333,444">
+  </Season>
+</document>
